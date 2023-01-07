6bd3a5d2763927a6e90e073378425e0d8a54d89d
hansani111 <hansanideshmukh894@gmail.com>
2023-01-07 17:46:07 +0530

    added: start the game

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,6 +2,9 @@ package com.bridgelabz;
 
 public class GamblingSimulation {
 
+    static int stake = 100;
+    static final int BET = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation problems");
     }
